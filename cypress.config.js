f19cea1ca37e92f7e1ed36cd78c6ed19cfa9cd31
nzainuddin <nadzainuddin@gmail.com>
2022-11-06 01:57:36 +0800

    Add page object model

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl : 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
